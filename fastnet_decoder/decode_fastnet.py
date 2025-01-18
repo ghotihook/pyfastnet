73457bc3b9689ebeca5361e95dfbd543a7ab705e
ghotihook <alex@ivila.net>
2025-01-18 21:17:01 +1100

    fixed formating for 0x0a

--- fastnet_decoder/decode_fastnet.py
+++ fastnet_decoder/decode_fastnet.py
@@ -260,7 +260,7 @@ def decode_format_and_data(channel_id, format_byte, data_bytes):
             second_value = int.from_bytes(data_bytes[2:], byteorder="big", signed=True)  # Second 16-bit signed integer
             interpreted_first_value = first_value / divisor
             interpreted_second_value = second_value / divisor
-            interpreted_value = {interpreted_first_value}
+            interpreted_value = interpreted_first_value
             raw_value = {"first": interpreted_first_value, "second": interpreted_second_value}
 
 
